7e2e4b0e6291a99a946962d2550a2048bf201a04
pmagdum <pmagdum@gmail.com>
2016-07-13 15:45:51 +0530

    Update DemoA.java

--- src/qsp/DemoA.java
+++ src/qsp/DemoA.java
@@ -7,6 +7,8 @@ public static void main(String args[])
 	System.out.println("Hi.. bye......");
 	System.out.println("welcome to SVN repository....");
 	System.out.println("from praveen eclipse to pri github");
+	System.out.println("from pri github to praveen eclipse");
+	System.out.println("Praveen, you r very nalayak......");
 }
 
 }
