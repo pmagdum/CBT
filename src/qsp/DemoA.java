a5a05fb0da151dbf99dbcb1e173bac012e97a6a5
cnspraveen <cns.praveen@gmail.com>
2016-07-13 15:43:51 +0530

    keep silence....don't talk much

--- src/qsp/DemoA.java
+++ src/qsp/DemoA.java
@@ -6,6 +6,7 @@ public static void main(String args[])
 {
 	System.out.println("Hi.. bye......");
 	System.out.println("welcome to SVN repository....");
+	System.out.println("from praveen eclipse to pri github");
 }
 
 }
